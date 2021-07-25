dd7f50bec31ab8c74e2a2d4635c4c08e0d2a40b9
Bun133 <bun133.work@gmail.com>
2021-07-25 21:13:11 +0900

    nanndenohonngoutenaino

--- main/src/main/java/com/github/bun133/nngraphics/display/Pos.kt
+++ main/src/main/java/com/github/bun133/nngraphics/display/Pos.kt
@@ -19,12 +19,6 @@ class Rect(var left_up: Pos, var right_down: Pos) {
         r.maxY.roundToInt()
     )
 
-    init {
-        if (right_down.x < left_up.x || left_up.y > right_down.y) {
-            throw Exception("Illegal Rect Pos left_up.x:${left_up.x} left_up.y:${left_up.y} right_down.x:${right_down.x} right_down.y:${right_down.y}l")
-        }
-    }
-
     fun width() = right_down.x - left_up.x
     fun height() = right_down.y - left_up.y
 
